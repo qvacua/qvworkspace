98cbb6276d12bd89f20e3b44897fc7a2e936aa9e
Tae Won Ha <h@taewon.de>
2015-09-13 12:30:14 +0200

    reformat

--- QVWorkspace/QVWorkspace.h
+++ QVWorkspace/QVWorkspace.h
@@ -1,10 +1,11 @@
-//
-//  QVWorkspace.h
-//  QVWorkspace
-//
-//  Created by Tae Won Ha on 13/09/15.
-//  Copyright (c) 2015 Tae Won Ha. All rights reserved.
-//
+/**
+* Tae Won Ha — @hataewon
+*
+* http://taewon.de
+* http://qvacua.com
+*
+* See LICENSE
+*/
 
 #import <Cocoa/Cocoa.h>
 
@@ -15,8 +16,6 @@ FOUNDATION_EXPORT double QVWorkspaceVersionNumber;
 FOUNDATION_EXPORT const unsigned char QVWorkspaceVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <QVWorkspace/PublicHeader.h>
-
-
 #import <QVWorkspace/QVWorkspaceView.h>
 #import <QVWorkspace/QVWorkspaceDelegate.h>
 #import <QVWorkspace/QVToolbarButtonDelegate.h>
